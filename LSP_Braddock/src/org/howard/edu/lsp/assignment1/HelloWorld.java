658346ec89fe7932357173afb1d2996a6e35bea5
sarabraddock <sarabraddock@saras-mbp>
2022-09-02 11:36:56 -0400

    My commit for assignment #1.

---
+++ LSP_Braddock/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Sara Braddock
+ * @author sarabraddock
+ *
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Large Scale Programming!! I'm Sara Braddock, junior, computer science major. ");
+	}
+
+}
+ 
