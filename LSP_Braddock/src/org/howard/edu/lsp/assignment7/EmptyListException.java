3e89ab47f77e8f683a6be2451bb6da33311c2dad
sarabraddock <sarabraddock@localhost>
2022-11-23 21:14:23 -0500

    Assignment #7

---
+++ LSP_Braddock/src/org/howard/edu/lsp/assignment7/EmptyListException.java
@@ -0,0 +1,15 @@
+/**
+ * @author sarabraddock
+ */
+
+package org.howard.edu.lsp.assignment7;
+
+public class EmptyListException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public EmptyListException(String errorMessage) { 
+		super(errorMessage);
+	}
+
+}
---
+++ LSP_Braddock/src/org/howard/edu/lsp/assignment7/HighAverageGrade.java
@@ -0,0 +1,43 @@
+/**
+ * @author sarabraddock
+ */
+
+package org.howard.edu.lsp.assignment7;
+
+import java.util.ArrayList;
+
+public class HighAverageGrade implements AverageStrategy {
+	/**
+	 * Helper function to return the list without the two lowest scores
+	 */
+	public ArrayList <Integer> lowestScores (ArrayList<Integer> grades){
+		for (int j = 0; j < 2; j++) {
+			int min = grades.get(0);
+			for (int i = 0; i < grades.size(); i++) {
+				if (grades.get(i) < min) {
+					min = grades.get(i);
+				}
+			}
+			grades.remove(grades.indexOf(min));
+		}
+		return grades;
+	}
+	
+	public int compute(ArrayList<Integer> grades) throws EmptyListException{
+		if (grades.size() == 0) {
+			throw new EmptyListException("List is Empty");
+		}
+		else {
+			grades = lowestScores(grades);
+			int sum = 0;
+			for (int i = 0; i < grades.size(); i++) {
+				sum = sum + grades.get(i);
+			}
+			
+			int avg = sum / (grades.size());
+			return avg;
+		}
+		
+	}
+
+}
